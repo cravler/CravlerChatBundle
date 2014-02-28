0098a9b5bedd74cb6151fdf6e49405a4a54df34a
cravler <sergeizz@gmail.com>
2014-02-28 09:44:41 +0200

    services.js improvements

--- Resources/public/js/services.js
+++ Resources/public/js/services.js
@@ -12,17 +12,6 @@
         };
     };
 
-    var call = function(ename, args) {
-        CravlerRemote.endpointsReady(function() {
-            if (CravlerRemote.endpointExists(ename)) {
-                var endpoint = jUtil.ns(ename);
-                endpoint.apply(endpoint, args);
-            } else {
-                console.error('Endpoint "' + ename + '" not defined!');
-            }
-        });
-    };
-
     app.factory('notification', function ($rootScope) {
         return {
             on: function(ename, cb) {
@@ -32,13 +21,13 @@
     });
 
     app.factory('chat', function ($rootScope) {
-        var prefix = 'Endpoints.CravlerChat_Chat.';
+        var prefix = 'CravlerChat_Chat.';
         return {
             changeName: function(name, cb) {
-                call(prefix + 'changeName', [name, wrapper($rootScope, cb)]);
+                CravlerRemote.invoke(prefix + 'changeName', name, wrapper($rootScope, cb));
             },
             sendMessage: function(message, cb) {
-                call(prefix + 'sendMessage', [message, wrapper($rootScope, cb)]);
+                CravlerRemote.invoke(prefix + 'sendMessage', message, wrapper($rootScope, cb));
             }
         };
     });
